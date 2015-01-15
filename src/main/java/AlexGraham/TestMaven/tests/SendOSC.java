fded4e31a2f2f89f27954c06b9ac2c42aaf845a5
Alex Graham <alex@alexgraham.net>
2015-01-14 21:27:11 -0500

    Moved tests from App

--- src/main/java/AlexGraham/TestMaven/tests/SendOSC.java
+++ src/main/java/AlexGraham/TestMaven/tests/SendOSC.java
@@ -6,6 +6,7 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
+import com.illposed.osc.OSCListener;
 import com.illposed.osc.OSCMessage;
 import com.illposed.osc.OSCPortIn;
 import com.illposed.osc.OSCPortOut;
@@ -32,6 +33,92 @@ public class SendOSC {
 		}
     	
     	System.out.println("Sent Message");
+	}
+	
+	public static void testOSC() throws IOException {
+    	OSCPortIn receiver = new OSCPortIn(1292);
+		
+    	OSCListener listener = new OSCListener() {
+    		public void acceptMessage(java.util.Date time, OSCMessage message) {
+
+    			System.out.println(message.getArguments());
+    			
+    	    	// Send
+    	    	OSCPortOut sender;
+				try {
+					sender = new OSCPortOut(InetAddress.getLocalHost(), 57120);
+	    	    	
+					ArrayList<Object> arguments = new ArrayList<Object>();
+	    	    	arguments.add(new Integer(3));
+	    	    	arguments.add("hello");
+	    	    	OSCMessage msg = new OSCMessage("/good/news", arguments);
+					
+					sender.send(msg);
+				} catch (SocketException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnknownHostException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+    			System.out.println(" ");
+    		}
+    		
+    	};
+    	
+    	
+    	receiver.addListener("/sayhello", listener);
+    	receiver.startListening();
+    	
+    	// Send
+    	OSCPortOut sender = new OSCPortOut(InetAddress.getLocalHost(), 23232);
+    	ArrayList<Object> arguments = new ArrayList<Object>();
+    	arguments.add(new Integer(3));
+    	arguments.add("hello");
+    	OSCMessage msg = new OSCMessage("/goodbye", arguments);
+
+    	try {
+			sender.send(msg);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	// Send
+		try {
+			sender = new OSCPortOut(InetAddress.getLocalHost(), 1292);
+	    	
+			arguments = new ArrayList<Object>();
+	    	arguments.add(new Integer(3));
+	    	arguments.add("hello");
+	    	msg = new OSCMessage("/sayhello", arguments);
+			
+			sender.send(msg);
+		} catch (SocketException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    	 // receive
+    	
+
+    	System.out.println("Listening");
+
+    	System.in.read();
+
+    	receiver.close();
+    	
+        System.out.println( "Done." );
 	}
 
 }
