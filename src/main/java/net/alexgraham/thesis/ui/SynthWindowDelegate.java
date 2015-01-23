5bf81163df2c8fd5e08b6a4043525bafc8bded81
Alex Graham <alex@alexgraham.net>
2015-01-23 14:28:53 -0500

    Moved files

--- src/main/java/net/alexgraham/thesis/ui/SynthWindowDelegate.java
+++
@@ -1,8 +0,0 @@
-package net.alexgraham.thesis.ui;
-
-import net.alexgraham.thesis.supercollider.SynthDef;
-
-public interface SynthWindowDelegate {
-	void selectSynth();
-	void launchSynth(SynthDef synthDef);
-}
