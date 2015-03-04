e68407a6dc92bace972c27b8e0e648e46c684abc
Alex Graham <alex@alexgraham.net>
2015-03-04 16:33:49 -0500

    Files moved

---
+++ src/main/java/net/alexgraham/thesis/supercollider/synths/parameters/models/ParamModel.java
@@ -0,0 +1,29 @@
+package net.alexgraham.thesis.supercollider.synths.parameters.models;
+
+import javax.swing.event.ChangeListener;
+
+import net.alexgraham.thesis.supercollider.synths.Instance;
+import net.alexgraham.thesis.supercollider.synths.parameters.Param;
+import net.alexgraham.thesis.ui.connectors.Connector;
+import net.alexgraham.thesis.ui.connectors.Connector.ConnectorType;
+
+public interface ParamModel {
+	public Instance getOwner();
+	public void setOwner(Instance owner);
+
+	public String getName();
+	public void setName(String name);
+
+	public Connector getConnector(ConnectorType type);
+	public void addConnector(ConnectorType type);
+	
+	public Object getObjectValue();
+		
+	/**
+	 * Adds a change listener to this model to update the instance whenever the model is updated
+	 * @param instance
+	 */
+	public void addInstanceListener(Instance instance);
+	
+	public void updateBounds(Param newParam); //TODO: Not always bounds
+}
